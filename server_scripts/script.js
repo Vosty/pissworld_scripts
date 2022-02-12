8dead5aa7a09900b01e6b55b89ca827a56fce115
Matthew Vosters <mattvosty@gmail.com>
2022-02-12 12:26:41 +0900

    Dark Souls item logic (red-eye, white soapdstone) progress

--- server_scripts/script.js
+++ server_scripts/script.js
@@ -6,14 +6,22 @@ settings.logSkippedRecipes = false
 settings.logErroringRecipes = true
 
 //CONFIGS
+const TEST_MODE = false //Allows players to do things like invade themselves
+
 const COMMAND_PREFIX = '!'
 
 const WHITE_SOAPSTONE_COMMAND = 'join'
 const WHITE_SOAPSTONE_SUMMON_TIME_IN_TICKS = 60
 const WHITE_SOAPSTONE_TIMEOUT_TIME_IN_TICKS = 300
 
+const REDEYE_INVASION_TIME_IN_TICKS = 160
+const REDEYE_INVASION_DISTANCE_FROM_PLAYER = 45
+const REDEYE_INVSASION_MIN_DISTANCE_FROM_PLAYER = 16
+
 let summon_sign_pos
 
+
+let PI = 3.141592653
 
 
 console.info('Hello, World! (You will see this line every time server resources reload)')
@@ -82,7 +90,7 @@ onEvent('item.right_click', event => {
 	}
 
 	// Soapstone draw mark
-	if (item.id === 'kubejs:white_soapstone' && event.hand == MAIN_HAND) {
+	if (item.id === 'kubejs:white_soapstone' /*&& event.hand == MAIN_HAND*/) {
 		let player = event.getPlayer()
 		let lookingAt = player.rayTrace(player.reachDistance)
 		if (lookingAt && lookingAt.block && lookingAt.facing == 'up') {
@@ -90,11 +98,88 @@ onEvent('item.right_click', event => {
 			let above = lookingAt.block.up
 			//console.info(above)
 			above.set('kubejs:soapstone_mark')
-			player.damageHeldItem(MAIN_HAND, 10)
+			player.damageHeldItem(event.hand, 10)
 			console.info(`soapstone mark placed by ${player}`)
 		}
 	}
 
+	// Homeward bone
+	if (item.id === 'kubejs:homeward_bone') {
+		let player = event.getPlayer()
+		let spawnpoint = player.getSpawnLocation()
+		let hand = event.hand
+		event.server.runCommandSilent(`/tp ${player} ${spawnpoint.x} ${spawnpoint.y} ${spawnpoint.z}`)
+		if (hand == MAIN_HAND) {
+			player.getMainHandItem().count--
+		} else {
+			player.getOffHandItem().count--
+		}
+
+	}
+
+	// Cracked Redeye Orb
+	if (item === 'kubejs:cracked_redeye_orb') {
+		let player = event.player
+		let allPlayers = event.server.players
+		let worldsToInvade = allPlayers.filter(function(value) {
+			return value.name == player.name || TEST_MODE
+		})
+		if (worldsToInvade.length == 0) {
+			player.tell('No worlds to invade')
+			return
+		}
+		let rand = Math.round(Math.random() * (worldsToInvade.length-1))
+		console.info(rand)
+		let invaded = worldsToInvade[rand]
+		console.info(`${invaded} is being invaded!`)
+		console.info(`${invaded} is at ${invaded.x} ${invaded.y} ${invaded.z}`)
+		player.tell('Searching for worlds to invade...')
+
+		//find proper spot to invade player
+		let randomAngle = Math.random() * PI * 2.0
+		console.info(randomAngle)
+		let goodPlaceX
+		let goodPlaceY = invaded.y
+		let goodPlaceZ
+		let placeFound = false
+		for (let dist = REDEYE_INVASION_DISTANCE_FROM_PLAYER; dist >= REDEYE_INVSASION_MIN_DISTANCE_FROM_PLAYER; dist--) {
+			//console.info(`checking distance: ${dist}`)
+			for (let a = 0.0; a < PI * 2.0 + randomAngle; a = a+1.0) {
+				//console.long(checking angle)
+				goodPlaceX = dist * Math.cos(a + randomAngle) + invaded.x
+				goodPlaceZ = dist * Math.sin(a + randomAngle) + invaded.z
+
+				let block = world.getBlock(goodPlaceX, goodPlaceY, goodPlaceZ)
+				if (block === 'minecraft:air' && block.up === 'minecraft:air' && block.down !== 'minecraft:air') {
+					placeFound = true
+					break
+				}
+			}
+			if (placeFound) {
+				console.info(`Distance to player: ${dist}`)
+				break
+			}
+		}
+
+		if (placeFound) {
+			event.server.scheduleInTicks(REDEYE_INVASION_TIME_IN_TICKS, function(callback) {
+				invaded.tell(`Dark spirit ${player} has invaded!`)
+				player.tell(`Invaded world of ${invaded}`)
+				console.info(`Teleporting ${player} to ${goodPlaceX} ${goodPlaceY} ${goodPlaceZ}`)
+				event.server.runCommandSilent(`/tp ${player} ${goodPlaceX} ${goodPlaceY} ${goodPlaceZ}`)
+			})
+		} else {
+			console.info('invasion failed')
+			player.tell('Failed to find fair location near player')
+		}
+
+		let hand = event.hand
+		if (hand == MAIN_HAND) {
+			player.getMainHandItem().count--
+		} else {
+			player.getOffHandItem().count--
+		}
+	}
 
 })
 
