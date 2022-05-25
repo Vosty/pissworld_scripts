45707f626d1f9ecea46a01e1c970d2bffb6d3e71
Matthew Vosters <mattvosty@gmail.com>
2022-05-25 12:45:31 +0900

    Added message to beer / ring slots interaction

--- server_scripts/script.js
+++ server_scripts/script.js
@@ -457,6 +457,8 @@ onEvent('item.food_eaten', function(event) {
 	    // Add the stage
 	    event.player.stages.add('beer_drank')
 	    // Give the player a a ring slot
+	    event.player.tell('As you drink your first beer, you feel yourself someone more in tune with the world and its magic')
+	    event.player.tell('You can now handle wearing one additional ring')
 	    event.server.runCommandSilent(`/curios add ring ${event.player} 1`)
   	}
 	}
